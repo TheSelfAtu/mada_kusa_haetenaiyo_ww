40f094ef3b50737d3aac96d28d6d7cebe2681119
TheSelfAtu <r673774d72k77e@gmail.com>
2022-03-26 23:16:39 +0900

    A: コントリビューションの取得機能を追加

--- main.ts
+++ main.ts
@@ -1,20 +1,26 @@
-import { graphql } from './graphql';
+import {ApolloClient, ApolloError, InMemoryCache, gql} from '@apollo/client/core';
+import 'cross-fetch/polyfill';  // グローバルな fetch 関数を定義する
+import * as dotenv from 'dotenv';
 
-// const QUERY = `
-//   {
-//     repository(owner: "octokit", name: "graphql.js") {
-//       issues(last: 3, states: [OPEN]) {
-//         edges {
-//           node {
-//             number
-//             title
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
-const QUERY = `
+// .env ファイルの内容を環境変数に反映
+dotenv.config();
+
+// const token = 'a4304a13bc6cdd52509c90a38a676fce962ce518';
+const token = process.env.MYAPP_GITHUB_TOKEN;
+if (typeof token === 'undefined') {
+    throw new Error('MYAPP_GITHUB_TOKEN cannot be found');
+}
+
+// GraphQL クライアントを生成
+const apolloClient = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {authorization: `Bearer ${token}`},
+  cache: new InMemoryCache(),
+});
+
+// 発行する GraphQL クエリ
+
+const QUERY = gql`
 query($userName:String!) {
     user(login: $userName){
       contributionsCollection {
@@ -32,18 +38,21 @@ query($userName:String!) {
   }
   `;
 
-async function main() {
-  try {
-    const {repository} = await graphql(QUERY,{userName: 'TheSelfAtu'});
-    console.log(repository,"repoo");
-    
-    // for (const {node: issue} of repository.issues.edges) {
-    //   console.log(`* ${issue.number}: ${issue.title}`);
-    // }
-  } catch (err) {
-    // console.error("err");
-    console.error(err);
-  }
+
+
+// クエリを発行
+apolloClient.query({query: QUERY,variables:{userName:"TheSelfAtu"}})
+  .then(result => handleApolloResult(result.data))
+  .catch(handleApolloError);
+
+// GraphQL レスポンスをハンドル
+function handleApolloResult(data: any) {
+  console.log(data);
+  console.log(data.contributionCalendar);
+  
 }
 
-main();
+// GraphQL のエラーをハンドル
+function handleApolloError(err: ApolloError) {
+  console.error(err.message);
+}
