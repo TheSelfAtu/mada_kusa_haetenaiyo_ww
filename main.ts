c71abb5a934f977b4b5c5599caab965da8486778
TheSelfAtu <r673774d72k77e@gmail.com>
2022-04-12 23:36:11 +0900

    A: 今日のコントリビューション数が取得できるように実装

--- main.ts
+++ main.ts
@@ -1,28 +1,33 @@
-import {ApolloClient, ApolloError, InMemoryCache, gql} from '@apollo/client/core';
-import 'cross-fetch/polyfill';  // グローバルな fetch 関数を定義する
-import * as dotenv from 'dotenv';
+import {
+  ApolloClient,
+  ApolloError,
+  InMemoryCache,
+  gql,
+} from "@apollo/client/core";
+import "cross-fetch/polyfill"; // グローバルな fetch 関数を定義する
+import * as dotenv from "dotenv";
 
 // .env ファイルの内容を環境変数に反映
 dotenv.config();
 
 // const token = 'a4304a13bc6cdd52509c90a38a676fce962ce518';
 const token = process.env.MYAPP_GITHUB_TOKEN;
-if (typeof token === 'undefined') {
-    throw new Error('MYAPP_GITHUB_TOKEN cannot be found');
+if (typeof token === "undefined") {
+  throw new Error("MYAPP_GITHUB_TOKEN cannot be found");
 }
 
 // GraphQL クライアントを生成
 const apolloClient = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  headers: {authorization: `Bearer ${token}`},
+  uri: "https://api.github.com/graphql",
+  headers: { authorization: `Bearer ${token}` },
   cache: new InMemoryCache(),
 });
 
 // 発行する GraphQL クエリ
 
 const QUERY = gql`
-query($userName:String!) {
-    user(login: $userName){
+  query ($userName: String!) {
+    user(login: $userName) {
       contributionsCollection {
         contributionCalendar {
           totalContributions
@@ -36,20 +41,29 @@ query($userName:String!) {
       }
     }
   }
-  `;
-
-
+`;
 
 // クエリを発行
-apolloClient.query({query: QUERY,variables:{userName:"TheSelfAtu"}})
-  .then(result => handleApolloResult(result.data))
+apolloClient
+  .query({ query: QUERY, variables: { userName: "TheSelfAtu" } })
+  .then((result) => handleApolloResult(result.data))
   .catch(handleApolloError);
 
 // GraphQL レスポンスをハンドル
 function handleApolloResult(data: any) {
-  console.log(data);
-  console.log(data.contributionCalendar);
-  
+  const weekCountThisYear =
+    data.user.contributionsCollection.contributionCalendar.weeks.length;
+  const dayCountThisWeek =
+    data.user.contributionsCollection.contributionCalendar.weeks[
+      weekCountThisYear - 1
+    ].contributionDays.length;
+
+  const contributionThisDay =
+    data.user.contributionsCollection.contributionCalendar.weeks[
+      weekCountThisYear - 1
+    ].contributionDays[dayCountThisWeek - 1];
+  const contributionCountThisDay = contributionThisDay.contributionCount;
+  console.log(contributionCountThisDay);
 }
 
 // GraphQL のエラーをハンドル
